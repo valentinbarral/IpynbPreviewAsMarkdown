5c6943ab45a68a7efdb3d979699a80317251a5bb
Valentin Barral <valentin.barral@udc.es>
2024-03-08 19:20:45 +0100

    Multiple improvements, added support for hiding cells by type.

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 
 # Ipynb Preview as Markdown
 
-"Ipynb Preview as Markdown" is a plugin for Sublime Text that enables users to view `.ipynb` (Jupyter Notebook) files as Markdown directly within the editor. This plugin provides a convenient way to preview notebook files without leaving Sublime Text, displaying the Markdown and code cells in a readable format.
+**Ipynb Preview as Markdown** is a plugin for Sublime Text that enables users to view `.ipynb` (Jupyter Notebook) files as Markdown directly within the editor. This plugin provides a convenient way to preview notebook files without leaving Sublime Text, displaying the Markdown, source code, and output cells in a readable format.
+
+## Features
+
+Below is an overview of the capabilities integrated into this plugin:
+
+- **Selective Cell Omission**: Users have the flexibility to exclude specific cells from the final Markdown document. This includes the ability to omit cells containing source code, those that display outputs following execution, or both.
+
+- **Image Handling Enhancements**: To address the limitations of Base64 encoded images within notebooks, this tool automatically converts these images into references to temporary files. This approach not only makes the document more readable but also facilitates easier manipulation and sharing of the generated Markdown files.
+
+- **Absolute Image Paths in Markdown Cells**: For Markdown cells that include references to local images, the tool automatically replaces the original path with an absolute path. This adjustment ensures that images are correctly rendered when using document conversion plugins like [Pandoc](https://packagecontrol.io/packages/Pandoc).
+
+- **Language Preservation for Source Code Cells**: In recognition of the diverse programming languages that can be used within notebooks, this tool maintains the original language specified for code cells in the notebook when converting to Markdown. This commitment to preserving the source language ensures that the syntax highlighting and other language-specific features remain intact in the final document, providing a seamless transition from notebook to Markdown format.
+
 
 ## Installation
 
@@ -31,8 +44,11 @@ To use the "Ipynb Preview as Markdown" plugin:
 
 1. Open a `.ipynb` file in Sublime Text.
 2. Open the Command Palette using `Shift+Ctrl+P` (or `Cmd+Shift+P` on macOS).
-3. Type "Preview ipynb as Markdown" and select the command when it appears.
-4. A new window will open with the Markdown preview. The preview is read-only and cannot be edited.
+3. Type *Preview ipynb as Markdown* and select the command when it appears.
+4. A new window will open with the Markdown preview, showing all the types of cell (Markdown, Source code, or Result). The preview is read-only and cannot be edited.
+5. Type instead *Preview ipynb as Markdown (No output)* to ommit the cells with results in the preview.
+6. Type instead *Preview ipynb as Markdown (No code)* to ommit the cells with source code in the preview.
+6. Type instead *Preview ipynb as Markdown (No code, No output)* to show only the Markdown cells in the preview.
 
 ## License
 
--- ipynb_preview_as_markdown.py
+++ ipynb_preview_as_markdown.py
@@ -22,44 +22,65 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-
 import sublime
 import sublime_plugin
 import json
+import re
+import os.path
+import base64
+import tempfile
 
-class PreviewIpynbAsMarkdownCommand(sublime_plugin.WindowCommand):
-    def run(self):
+class PreviewIpynbAsMarkdownCommand(sublime_plugin.TextCommand):
 
-        active_view = self.window.active_view()
+    def run(self, edit, **args):
+
+        # Options
+        show_code = args['show_code']
+        show_output = args['show_output']
         
-        if not active_view:
-            return
+        file_name = self.view.file_name()
+        file_content = self.view.substr(sublime.Region(0, self.view.size()))
         
-        file_name = active_view.file_name()
-        if file_name and file_name.endswith('.ipynb'):
-            # Load the file's content
-            file_content = active_view.substr(sublime.Region(0, active_view.size()))
+        try:
+            nb = json.loads(file_content)
+            base_path = self.view.window().extract_variables()["file_path"]
+            markdown_content = ""
+
+            # Check metadata to find source code language
+            sc_lan = 'python'
+            if nb['metadata']['kernelspec']['language']:
+                sc_lan = nb['metadata']['kernelspec']['language']
+
+            # Convert cells to Markdown
+            for cell in nb['cells']:
+                if cell['cell_type'] == 'markdown':
+                    cell_content = ''.join(cell['source']) + '\n\n'
+                    markdown_content += self.image_relative_paths_to_abs(base_path, cell_content)
+                    #markdown_content += cell_content.replace('./', base_path+'/')
+
+                elif cell['cell_type'] == 'code':
+                    if show_code=='True':
+                        markdown_content += '```' + sc_lan + '\n' + ''.join(cell['source']) + '\n```\n\n'
+                    if show_output=='True':
+                        for output_node in cell['outputs']:
+                            markdown_content += self.parse_output_node(output_node)
             
-            try:
-                nb = json.loads(file_content)
-                markdown_content = ""
-                
-                # Convert cells to Markdown
-                for cell in nb['cells']:
-                    if cell['cell_type'] == 'markdown':
-                        markdown_content += ''.join(cell['source']) + '\n\n'
-                    elif cell['cell_type'] == 'code':
-                        markdown_content += '```python\n' + ''.join(cell['source']) + '\n```\n\n'
-                
-                # Create a new window to show the preview son the user's layout remain untouched
-                self.show_markdown_in_new_window(markdown_content)
-                
-            except Exception as e:
-                sublime.error_message("Error processing .ipynb file: " + str(e))
-        else:
-            sublime.error_message("The current file is not an .ipynb file.")
+            self.show_markdown_in_new_window(markdown_content)
+            
+        except Exception as e:
+            sublime.error_message("Error processing .ipynb file: " + str(e))
+
+    def is_enabled(self):
+        file_name = self.view.file_name()
+        if (file_name):
+            return file_name.endswith('.ipynb')
+        return False
 
     def show_markdown_in_new_window(self, markdown_content):
+        """
+        Creates a new window and shows the markdown code
+
+        """
         new_window = sublime.run_command('new_window')
         new_view = sublime.active_window().new_file()
         new_view.set_scratch(True)
@@ -67,3 +88,68 @@ class PreviewIpynbAsMarkdownCommand(sublime_plugin.WindowCommand):
         new_view.set_syntax_file('Packages/Markdown/Markdown.sublime-syntax')
         new_view.run_command('append', {'characters': markdown_content})
         new_view.set_read_only(True)
+
+
+    def get_markdown_from_temp_image(self, base64_image):
+        """
+        Saves a Base64 encoded image to temporary file and generates Markdown code for it.
+
+        """
+        # Decode the Base64 encoded image
+        image_data = base64.b64decode(base64_image)
+
+        # Create a temporary file for the image
+        image_file = tempfile.NamedTemporaryFile(delete=False, suffix=".png", mode="wb")
+        image_file.write(image_data)
+        
+        # Return Markdown code to display the image
+        return "![]({0})".format(image_file.name)
+
+
+    def image_relative_paths_to_abs(self, base_path, original_text):
+        """
+        Changes relative references to images to absolute paths
+
+        """
+        # Pattern to find markdown image references, e.g., ![caption](image)
+        pattern = r'!\[(.*?)\]\((.*?)\)'
+        
+        # Function to replace the image reference with its full path
+        def replace_with_full_path(match):
+            image_reference = match.group(2)
+            full_path = os.path.join(base_path, image_reference)
+            return '![{0}]({1})'.format(match.group(1),full_path)
+        
+        # Replace all found image references in the markdown text
+        modified = re.sub(pattern, replace_with_full_path, original_text)
+        
+        return modified
+
+    def parse_output_node(self, output_node):
+        """
+        Parses the output of a source code node (text, image, or stream)
+
+        """
+        result = ""
+        if output_node['output_type']=='execute_result':
+            data = output_node['data']
+            text_items = data['text/plain']
+            for item in text_items:
+                result += ''.join(item) + '\n'
+        elif output_node['output_type']=='display_data':
+            data = output_node['data']
+            image_64 = data['image/png']
+            text_items = data['text/plain']
+            if image_64:
+                result += self.get_markdown_from_temp_image(image_64) + '\n'
+        elif output_node['output_type']=='stream':
+            text_items = output_node['text']
+            for item in text_items:
+                result += ''.join(item)+ '\n'
+
+        return result + '\n\n'
+
+
+
+
+ 
